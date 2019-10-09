a22fef65184a1d55db99f8f930d9eb901578a5dd
Brian Souza <brian.diego.souza@gmail.com>
2019-10-08 22:50:07 -0300

    Initial Commit

---
+++ SuperSenha/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SuperSenha
+//
+//  Created by Brian Diego De Souza on 08/10/19.
+//  Copyright © 2019 Brian Diego De Souza. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
