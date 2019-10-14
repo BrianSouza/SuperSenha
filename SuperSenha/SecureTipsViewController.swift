10ca6d01498a67f3d2b3ae94245a0529a7ad45c1
Brian Souza <brian.diego.souza@gmail.com>
2019-10-14 20:53:05 -0300

    Fim do app da seção 5 do curso

---
+++ SuperSenha/SecureTipsViewController.swift
@@ -0,0 +1,23 @@
+//
+//  SecureTipsViewController.swift
+//  SuperSenha
+//
+//  Created by Brian Diego De Souza on 10/10/19.
+//  Copyright © 2019 Brian Diego De Souza. All rights reserved.
+//
+
+import UIKit
+
+class SecureTipsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    @IBAction func Close(_ sender: UIButton) {
+        dismiss(animated:true,completion: nil)
+        
+    }
+    
+}
